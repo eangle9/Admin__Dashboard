6c0637a4231b2b87643a9fabb65e086b6b169c17
engdawork <engdaworkyismaw9@gmail.com>
2023-05-01 18:13:11 +0300

    theme color of the project is added to theme.js

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,28 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from '@mui/material/InputBase';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotifiationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+    return (
+        <Box display="flex" justifyContent="space-between" p={2}>
+            <Box
+                display="flex"
+                backgrounColor={colors.primary[400]}
+                borderRadius="3px">
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="search" />
+            </Box>
+        </Box>
+    );
+}
+
+export default Topbar;
